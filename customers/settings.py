77cc6613607d188ab6e40c18fb6c2d9bf5bd4cd4
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-12-18 15:03:53 +0100

    Add Cors config

--- customers/settings.py
+++ customers/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'customers',
     'django.contrib.admin',
@@ -41,7 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
