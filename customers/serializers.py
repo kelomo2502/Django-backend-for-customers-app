d204450a5a50e5b194ce6fe1b1ea873aa469d3f5
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-12-18 14:01:41 +0100

    Adding first get endpoint

---
+++ customers/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from customers.models import Customer
+class CustomerSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Customer
+    fields = '__all__'
